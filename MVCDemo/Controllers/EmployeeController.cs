ce90356eb587bb9f0b3d3008a3d6967905cb03a8
manish <manish.7151.sharma@gmail.com>
2021-12-03 01:35:00 +0530

    modelUpdates

--- MVCDemo/Controllers/EmployeeController.cs
+++ MVCDemo/Controllers/EmployeeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCDemo.Models;
 
 namespace MVCDemo.Controllers
 {
@@ -11,7 +12,17 @@ namespace MVCDemo.Controllers
         // GET: Employee
         public ActionResult Index()
         {
-            return View();
+            var data = AllEmployees();
+            return View(data);
+
+
+            //below if viewname is deiffrenet from action name
+            //return View("Viewname",data);
+        }
+
+        private Employee AllEmployees()
+        {
+            return new Employee { Id = 1, Name = "Manish", Address = "Kanpur" };
         }
     }
 }
